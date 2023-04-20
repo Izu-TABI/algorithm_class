ca45024276d7566bf671bafaa33e27e4607ec5a8
Izu-TABI <masakisama14@gmail.com>
2023-04-20 22:09:32 +0900

    Add:  algorithm sort

---
+++ bubblesort.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define N 5
+
+// 配列を表示させる関数
+void print_arr(int arr[], int cnt) {
+  for (int i = 0; i < N; i++) {
+    printf("%d ", arr[i]);
+  }
+
+  // カウンタ変数を表示させるか
+  if (cnt != -1) {
+    printf("(i = %d)\n", cnt);
+  } else {
+    printf("\n");
+  }
+}
+
+int main(void) {
+  int sort[N] = {2, 3, 1, 5, 4};
+  int tmp;           // tmp: 値を一時的に格納する
+  bool flag = false; // 値の交換が行われたかを判断する
+
+  printf("ソート前: ");
+
+  // 第二引数が-1の時はiの表示を行わない
+  print_arr(sort, -1);
+  do {
+    flag = false;
+    for (int i = 0; i < N - 1; i++) {
+      if (sort[i] > sort[i + 1]) {
+        flag = true;
+        tmp = sort[i];
+        sort[i] = sort[i + 1];
+        sort[i + 1] = tmp;
+      }
+
+      // 配列を表示、カウンタ変数を第二引数に指定
+      print_arr(sort, i);
+    }
+
+  } while (flag);
+  printf("ソート終了\n");
+
+  printf("ソート後: ");
+
+  // 第二引数が-1の時はiの表示を行わない
+  print_arr(sort, -1);
+
+  return 0;
+}
